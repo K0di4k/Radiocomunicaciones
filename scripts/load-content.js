5f7526c756176604669dbe2eaf8f171c14842805
K0di4k <jaime.galazlillo@gmail.com>
2025-01-24 16:21:24 -0300

    menus

--- scripts/load-content.js
+++ scripts/load-content.js
@@ -1,15 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Cargar header
-    fetch('./modulos/voltajes.html')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('botones').innerHTML = html;
-        });
-
-    // Cargar footer
-    fetch('./modulos/navbar.html')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('navbar').innerHTML = html;
-        });
-});
+document.getElementById('menu-voltajes').addEventListener('click', function(event) {
+    event.preventDefault(); 
+    
+    
+    fetch('../modulos/voltajes.html')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error al cargar el archivo');
+        }
+        return response.text();
+      })
+      .then(html => {
+        document.getElementById('botones').innerHTML = html;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  });
